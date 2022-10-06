e3acf9f608bc49990c7e72ab55d99f003992885c
Brcok7890 <nm1107@student.aru.ac.uk>
2022-10-06 10:29:56 +0100

    v0.2 - testing

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("test");
+string x = Console.ReadLine();
+if (x.Equals("yes")){Console.WriteLine("test2");}
 Console.ReadKey();
