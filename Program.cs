17831534a207c6a06016d2bfaf0df9038d513924
Brcok7890 <nm1107@student.aru.ac.uk>
2022-10-06 11:30:03 +0100

    v0.3 - try and swicth testing

--- Program.cs
+++ Program.cs
@@ -1,6 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("test");
-string x = Console.ReadLine();
-if (x.Equals("yes")){Console.WriteLine("test2");}
-Console.ReadKey();
+﻿Start:
+Console.Write("enter num between 1 and 7 (inclusive): ");
+try
+{
+    int x = Convert.ToInt32(Console.ReadLine());
+    switch (x)
+    {
+        case 1:
+            Console.WriteLine("Monday");
+            break;
+        case 2:
+            Console.WriteLine("Tuesday");
+            break;
+        case 3:
+            Console.WriteLine("Wednesday");
+            break;
+        case 4:
+            Console.WriteLine("Thursday");
+            break;
+        case 5:
+            Console.WriteLine("Friday");
+            break;
+        case 6:
+            Console.WriteLine("Saturday");
+            break;
+        case 7:
+            Console.WriteLine("Sunday");
+            break;
+        default:
+            Console.WriteLine("1-7 please");
+            break;
+    }
+}
+catch
+{
+    Console.WriteLine("incorrect input");
+}
+goto Start;
